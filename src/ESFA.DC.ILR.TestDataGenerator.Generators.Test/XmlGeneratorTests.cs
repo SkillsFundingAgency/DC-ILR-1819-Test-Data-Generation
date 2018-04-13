73d11370671df1accc8b5dac5427987fc30122e1
Hal Angseesing <tomlingames@gmail.com>
2018-04-13 15:40:46 +0100

    Added UnitTests for teh XmlGenerator assembly. Promoted namespace constants to public

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Generators.Test/XmlGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DCT.TestDataGenerator;
+using FluentAssertions;
+using Xunit;
+
+namespace DCT.TestDataGenerator.Test
+{
+    public class XmlGeneratorTests
+    {
+        [Fact]
+        public void CreateAllXml_NullArgumentToException()
+        {
+            int UKPRN = 8;
+            RuleToFunctorParser rfp = new RuleToFunctorParser(new DataCache());
+            XmlGenerator generator = new XmlGenerator(rfp, UKPRN);
+            Action call = () => { generator.CreateAllXml(null, 1, XmlGenerator.ESFA201819Namespace); };
+            call.Should().Throw<NullReferenceException>();
+        }
+    }
+}
