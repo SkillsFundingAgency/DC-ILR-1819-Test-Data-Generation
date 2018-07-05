06aa1cb1f1ad6bb66dc54bee9bbbfb011c1e864b
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-07-05 11:56:55 +0100

    44073 - AchDate_07

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/AchDate/AchDate_07.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class AchDate_07
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "AchDate_07";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "AchDt07";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherYP1619, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptions }
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                var lds = learner.LearningDelivery.ToList();
+                Helpers.SetLearningDeliveryEndDates(lds[0], lds[0].LearnStartDate.AddDays(30), Helpers.SetAchDate.SetAchDate);
+                lds[0].AchDate = DateTime.Today.AddDays(1);
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            _options = options;
+        }
+    }
+}
