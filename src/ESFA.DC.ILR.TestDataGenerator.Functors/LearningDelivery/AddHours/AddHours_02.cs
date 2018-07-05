71573cc985b956e6fde32ff8a465ac418559f77a
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-07-05 12:13:52 +0100

    43858 - AddHours_02

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/AddHours/AddHours_02.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class AddHours_02
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "AddHours_02";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "AddHr02";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.CommunityLearning, DoMutateLearner = MutateWithSOF, DoMutateOptions = MutateGenerationOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.YP1619, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.NonFunded, DoMutateLearner = MutateDOB, DoMutateOptions = MutateGenerationOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherYP1619, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Adult, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptions, ExclusionRecord = true }
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                foreach (var ld in learner.LearningDelivery)
+                {
+                    ld.AddHoursSpecified = true;
+                    ld.AddHours = 1;
+                }
+            }
+        }
+
+        private void MutateWithSOF(MessageLearner learner, bool valid)
+        {
+            Mutate(learner, valid);
+            Helpers.AddOrChangeLearningDeliverySourceOfFunding(learner.LearningDelivery[0], LearnDelFAMCode.SOF_ESFA_Adult);
+        }
+
+        private void MutateDOB(MessageLearner learner, bool valid)
+        {
+            Mutate(learner, valid);
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19);
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            _options = options;
+        }
+    }
+}
