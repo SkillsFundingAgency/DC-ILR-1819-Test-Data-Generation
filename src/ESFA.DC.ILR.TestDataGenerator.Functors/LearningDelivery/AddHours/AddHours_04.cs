7345c32a81bf471c39ddf335beaf761373e23f1b
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-07-05 12:15:59 +0100

    43868 - AddHours_04

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/AddHours/AddHours_04.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class AddHours_04
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "AddHours_04";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "AddHr04";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherYP1619, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherYP1619, DoMutateLearner = MutateAddHours, DoMutateOptions = MutateGenerationOptions, ExclusionRecord = true },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherYP1619, DoMutateLearner = MutateAddHoursLessThan60, DoMutateOptions = MutateGenerationOptions, ExclusionRecord = true }
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                foreach (var ld in learner.LearningDelivery)
+                {
+                    ld.LearnPlanEndDate = ld.LearnStartDate.AddDays(2);
+                    ld.AddHoursSpecified = true;
+                    ld.AddHours = 73;
+                }
+            }
+        }
+
+        private void MutateAddHours(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                foreach (var ld in learner.LearningDelivery)
+                {
+                    ld.LearnPlanEndDate = ld.LearnStartDate.AddDays(2);
+                    ld.AddHoursSpecified = true;
+                    ld.AddHours = 72;
+                }
+            }
+        }
+
+        private void MutateAddHoursLessThan60(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                foreach (var ld in learner.LearningDelivery)
+                {
+                    ld.LearnPlanEndDate = ld.LearnStartDate.AddDays(1);
+                    ld.AddHoursSpecified = true;
+                    ld.AddHours = 50;
+                }
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            _options = options;
+        }
+    }
+}
