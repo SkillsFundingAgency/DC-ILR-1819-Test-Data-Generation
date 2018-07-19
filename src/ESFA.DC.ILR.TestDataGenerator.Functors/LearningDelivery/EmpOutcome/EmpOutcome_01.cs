b02cab9a7fc95ae7a4e448830bffe9bbf9d2c705
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-07-19 13:23:30 +0100

    43933 - EmpOutcome_01, 43938 - EmpOutcome_02 rules added

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/EmpOutcome/EmpOutcome_01.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class EmpOutcome_01
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.None;
+        }
+
+        public string RuleName()
+        {
+            return "EmpOutcome_01";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "EmpOut_01";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Adult, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptionsProgression, ExclusionRecord = true },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.NonFunded, DoMutateLearner = MutateDOB, DoMutateOptions = MutateGenerationOptionsProgression },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.YP1619, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptionsProgression },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherYP1619, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptionsProgression },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.CommunityLearning, DoMutateLearner = MutateDOB, DoMutateOptions = MutateGenerationOptionsSOF },
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                foreach (MessageLearnerLearningDelivery ld in learner.LearningDelivery)
+                {
+                    ld.EmpOutcomeSpecified = true;
+                    ld.EmpOutcome = 1;
+                }
+            }
+        }
+
+        private void MutateDOB(MessageLearner learner, bool valid)
+        {
+           var learnerTypeRequired = learner.LearningDelivery[0].FundModel;
+            switch (learnerTypeRequired)
+            {
+                case 99:
+                    learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(1);
+                    break;
+
+                case 10:
+                    learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+                    break;
+            }
+
+           Mutate(learner, valid);
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            options.EmploymentRequired = true;
+        }
+
+        private void MutateGenerationOptionsSOF(GenerationOptions options)
+        {
+            options.LD.IncludeSOF = true;
+        }
+
+        private void MutateGenerationOptionsProgression(GenerationOptions options)
+        {
+            options.CreateDestinationAndProgression = true;
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/EmpOutcome/EmpOutcome_02.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class EmpOutcome_02
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.None;
+        }
+
+        public string RuleName()
+        {
+            return "EmpOutcome_02";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "EmpOut_02";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Adult, DoMutateLearner = MutateESM, DoMutateOptions = MutateGenerationOptionsProgression, InvalidLines = 2 },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.NonFunded, DoMutateLearner = MutateEmpOutcome, DoMutateOptions = MutateGenerationOptionsProgression, InvalidLines = 2, ExclusionRecord = true },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherAdult, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptionsProgression, InvalidLines = 2 },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.ESF, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptionsProgression, InvalidLines = 2 },
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            learner.LearningDelivery[0].EmpOutcomeSpecified = true;
+            learner.LearningDelivery[1].EmpOutcomeSpecified = true;
+            learner.LearningDelivery[0].EmpOutcome = 1;
+            learner.LearningDelivery[1].EmpOutcome = 2;
+
+            if (!valid)
+            {
+                learner.LearningDelivery[0].EmpOutcome = 7;
+                learner.LearningDelivery[1].EmpOutcome = 8;
+            }
+        }
+
+        private void MutateESM(MessageLearner learner, bool valid)
+        {
+            var lesm = learner.LearnerEmploymentStatus[0].EmploymentStatusMonitoring.ToList();
+
+            lesm.Add(new MessageLearnerLearnerEmploymentStatusEmploymentStatusMonitoring()
+            {
+                ESMType = EmploymentStatusMonitoringType.BSI.ToString(),
+                ESMCode = (int)EmploymentStatusMonitoringCode.BenefitEmploymentSupport,
+                ESMCodeSpecified = true
+            });
+            learner.LearnerEmploymentStatus[0].EmploymentStatusMonitoring = lesm.ToArray();
+            Mutate(learner, valid);
+        }
+
+        private void MutateEmpOutcome(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(1);
+            if (!valid)
+            {
+                learner.LearningDelivery[0].EmpOutcomeSpecified = true;
+                learner.LearningDelivery[1].EmpOutcomeSpecified = true;
+                learner.LearningDelivery[0].EmpOutcome = 7;
+                learner.LearningDelivery[1].EmpOutcome = 8;
+            }
+        }
+
+        private void MutateGenerationOptionsProgression(GenerationOptions options)
+        {
+            options.LD.GenerateMultipleLDs = 2;
+            options.CreateDestinationAndProgression = true;
+        }
+    }
+}
