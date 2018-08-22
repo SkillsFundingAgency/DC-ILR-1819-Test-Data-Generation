ea8407005956942f2439e88c1361f4625d0cec37
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-08-22 13:42:36 +0100

    43723 - ProgType_01, 43728 - ProgType_02,43743- ProgType_06

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/ProgType/ProgType_02.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class ProgType_02
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.None;
+        }
+
+        public string RuleName()
+        {
+            return "ProgType_02";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "PrgTyp02";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherAdult, DoMutateLearner = MutateLearner, DoMutateOptions = MutateGenerationOptions }
+            };
+        }
+
+        private void MutateLearner(MessageLearner learner, bool valid)
+        {
+            var ld = learner.LearningDelivery[0];
+            if (!valid)
+            {
+                ld.ProgTypeSpecified = true;
+                ld.ProgType = (int)ProgType.Traineeship;
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+        }
+    }
+}
