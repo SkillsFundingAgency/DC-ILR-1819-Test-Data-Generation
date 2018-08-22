ecf8fef9c41c134ec497d9aec50c444c4a2c7c2f
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-08-22 13:46:10 +0100

    Merge to develop

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/ProgType/ProgType_01.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class ProgType_01
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.None;
+        }
+
+        public string RuleName()
+        {
+            return "ProgType_01";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "PrgTyp01";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateLearner, DoMutateOptions = MutateGenerationOptions }
+            };
+        }
+
+        private void MutateLearner(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                foreach (var ld in learner.LearningDelivery)
+                {
+                    ld.ProgTypeSpecified = false;
+                }
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+        }
+    }
+}
