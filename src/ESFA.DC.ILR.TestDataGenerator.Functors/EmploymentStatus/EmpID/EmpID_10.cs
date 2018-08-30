4069c5bcf4c1a973b3356012fc24b908159d4738
Hema Jesu <Hema.Jesu@hotmail.com>
2018-08-30 11:21:41 +0100

    42475 - R30/ 42480 -R31 / 42555 - R66 / 42615 -R91 / 42354  EMpID 10,13, EmpStat_09 - Add new method to Helper

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/EmploymentStatus/EmpID/EmpID_10.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class EmpID_10
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "EmpID_10";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "EmpID10";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateLES, DoMutateOptions = MutateGenerationOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherAdult, DoMutateLearner = MutateLES, DoMutateOptions = MutateGenerationOptions, ExclusionRecord = true }
+            };
+        }
+
+        private void MutateLES(MessageLearner learner, bool valid)
+        {
+            learner.LearningDelivery[0].LearnStartDate = DateTime.Now.AddMonths(-1);
+            if (!valid)
+            {
+                var les = learner.LearnerEmploymentStatus[0];
+                les.EmpStatSpecified = true;
+                les.EmpStat = 10;
+                les.EmpIdSpecified = false;
+            }
+        }
+
+        private void MutateLDMType(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                var led = learner.LearningDelivery[0];
+                var ldfams = led.LearningDeliveryFAM.ToList();
+                ldfams.Add(new MessageLearnerLearningDeliveryLearningDeliveryFAM()
+                {
+                    LearnDelFAMType = LearnDelFAMType.LDM.ToString(),
+                    LearnDelFAMCode = ((int)LearnDelFAMCode.LDM_OLASS).ToString(),
+                });
+                led.LearningDeliveryFAM = ldfams.ToArray();
+                var les = learner.LearnerEmploymentStatus[0];
+                les.DateEmpStatAppSpecified = true;
+                les.DateEmpStatApp = new DateTime(2017, 12, 01);
+            }
+        }
+
+        private void MutateCommunity(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                var led = learner.LearningDelivery[0];
+                var ldfams = led.LearningDeliveryFAM.ToList();
+                ldfams.Add(new MessageLearnerLearningDeliveryLearningDeliveryFAM()
+                {
+                    LearnDelFAMType = LearnDelFAMType.SOF.ToString(),
+                    LearnDelFAMCode = ((int)LearnDelFAMCode.SOF_LA).ToString(),
+                });
+
+                led.LearningDeliveryFAM = ldfams.ToArray();
+                var les = learner.LearnerEmploymentStatus[0];
+                les.DateEmpStatAppSpecified = true;
+                les.DateEmpStatApp = new DateTime(2017, 12, 01);
+            }
+        }
+
+        private void MutateTrainee(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                learner.LearningDelivery[0].ProgType = 24;
+                learner.LearningDelivery[0].ProgTypeSpecified = true;
+                var les = learner.LearnerEmploymentStatus[0];
+                les.DateEmpStatAppSpecified = true;
+                les.DateEmpStatApp = new DateTime(2017, 12, 01);
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            options.EmploymentRequired = true;
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/EmploymentStatus/EmpID/EmpID_13.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class EmpID_13
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "EmpID_13";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "EmpID13";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateLES, DoMutateOptions = MutateGenerationOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.OtherAdult, DoMutateLearner = MutateLES, DoMutateOptions = MutateGenerationOptions, ExclusionRecord = true }
+            };
+        }
+
+        private void MutateLES(MessageLearner learner, bool valid)
+        {
+            learner.LearningDelivery[0].LearnStartDate = DateTime.Now.AddMonths(-1);
+            if (!valid)
+            {
+                learner.LearningDelivery[0].LearnStartDate = DateTime.Now.AddMonths(-2);
+                var les = learner.LearnerEmploymentStatus[0];
+                les.EmpIdSpecified = true;
+                les.EmpId = 999999999;
+            }
+        }
+
+        private void MutateLDMType(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                var led = learner.LearningDelivery[0];
+                var ldfams = led.LearningDeliveryFAM.ToList();
+                ldfams.Add(new MessageLearnerLearningDeliveryLearningDeliveryFAM()
+                {
+                    LearnDelFAMType = LearnDelFAMType.LDM.ToString(),
+                    LearnDelFAMCode = ((int)LearnDelFAMCode.LDM_OLASS).ToString(),
+                });
+                led.LearningDeliveryFAM = ldfams.ToArray();
+                var les = learner.LearnerEmploymentStatus[0];
+                les.DateEmpStatAppSpecified = true;
+                les.DateEmpStatApp = new DateTime(2017, 12, 01);
+            }
+        }
+
+        private void MutateCommunity(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                var led = learner.LearningDelivery[0];
+                var ldfams = led.LearningDeliveryFAM.ToList();
+                ldfams.Add(new MessageLearnerLearningDeliveryLearningDeliveryFAM()
+                {
+                    LearnDelFAMType = LearnDelFAMType.SOF.ToString(),
+                    LearnDelFAMCode = ((int)LearnDelFAMCode.SOF_LA).ToString(),
+                });
+
+                led.LearningDeliveryFAM = ldfams.ToArray();
+                var les = learner.LearnerEmploymentStatus[0];
+                les.DateEmpStatAppSpecified = true;
+                les.DateEmpStatApp = new DateTime(2017, 12, 01);
+            }
+        }
+
+        private void MutateTrainee(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                learner.LearningDelivery[0].ProgType = 24;
+                learner.LearningDelivery[0].ProgTypeSpecified = true;
+                var les = learner.LearnerEmploymentStatus[0];
+                les.DateEmpStatAppSpecified = true;
+                les.DateEmpStatApp = new DateTime(2017, 12, 01);
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            options.EmploymentRequired = true;
+        }
+    }
+}
