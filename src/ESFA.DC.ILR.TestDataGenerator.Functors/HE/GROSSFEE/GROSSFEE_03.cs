e6a808544aedd248e8595b57a6c4b80600b882a7
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-10-09 15:39:46 +0100

    43186 - GROSSFEE_01, 43191-GrossFee_02,43196 - GrossFee_03

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/HE/GROSSFEE/GROSSFEE_03.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class GROSSFEE_03
+        : ILearnerMultiMutator
+    {
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.None;
+        }
+
+        public string RuleName()
+        {
+            return "GROSSFEE_03";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "GrsFee03";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.NonFunded, DoMutateLearner = MutateHE, DoMutateOptions = MutateGenerationOptions }
+            };
+        }
+
+        public void MutateHE(MessageLearner learner, bool valid)
+        {
+            var ldhe = new List<MessageLearnerLearningDeliveryLearningDeliveryHE>();
+            if (valid)
+            {
+                ldhe.Add(new MessageLearnerLearningDeliveryLearningDeliveryHE()
+                {
+                    NUMHUS = "2000812012XTT60021",
+                    QUALENT3 = QualificationOnEntry.X06.ToString(),
+                    UCASAPPID = "AB89",
+                    TYPEYR = (int)TypeOfyear.FEYear,
+                    TYPEYRSpecified = true,
+                    MODESTUD = (int)ModeOfStudy.NotInPopulation,
+                    MODESTUDSpecified = true,
+                    FUNDLEV = (int)FundingLevel.Undergraduate,
+                    FUNDLEVSpecified = true,
+                    FUNDCOMP = (int)FundingCompletion.NotYetCompleted,
+                    FUNDCOMPSpecified = true,
+                    STULOAD = 10.0M,
+                    STULOADSpecified = true,
+                    YEARSTU = 1,
+                    YEARSTUSpecified = true,
+                    MSTUFEE = (int)MajorSourceOfTuitionFees.NoAward,
+                    MSTUFEESpecified = true,
+                    PCFLDCS = 100,
+                    PCFLDCSSpecified = true,
+                    SPECFEE = (int)SpecialFeeIndicator.Other,
+                    SPECFEESpecified = true,
+                    NETFEE = 9000,
+                    NETFEESpecified = true,
+                    GROSSFEE = 30000,
+                    GROSSFEESpecified = true,
+                    DOMICILE = "ZZ",
+                    ELQ = (int)EquivalentLowerQualification.NotRequired,
+                    ELQSpecified = true,
+                });
+            }
+
+            if (!valid)
+            {
+                ldhe.Add(new MessageLearnerLearningDeliveryLearningDeliveryHE()
+                {
+                    NUMHUS = "2000812012XTT60021",
+                    QUALENT3 = QualificationOnEntry.X06.ToString(),
+                    UCASAPPID = "AB89",
+                    TYPEYR = (int)TypeOfyear.FEYear,
+                    TYPEYRSpecified = true,
+                    MODESTUD = (int)ModeOfStudy.NotInPopulation,
+                    MODESTUDSpecified = true,
+                    FUNDLEV = (int)FundingLevel.Undergraduate,
+                    FUNDLEVSpecified = true,
+                    FUNDCOMP = (int)FundingCompletion.NotYetCompleted,
+                    FUNDCOMPSpecified = true,
+                    STULOAD = 10.0M,
+                    STULOADSpecified = true,
+                    YEARSTU = 1,
+                    YEARSTUSpecified = true,
+                    MSTUFEE = (int)MajorSourceOfTuitionFees.NoAward,
+                    MSTUFEESpecified = true,
+                    PCFLDCS = 100,
+                    PCFLDCSSpecified = true,
+                    SPECFEE = (int)SpecialFeeIndicator.Other,
+                    SPECFEESpecified = true,
+                    NETFEE = 9000,
+                    NETFEESpecified = true,
+                    GROSSFEE = 30001,
+                    GROSSFEESpecified = true,
+                    DOMICILE = "ZZ",
+                    ELQ = (int)EquivalentLowerQualification.NotRequired,
+                    ELQSpecified = true
+                });
+            }
+
+            learner.LearningDelivery[0].LearningDeliveryHE = ldhe.ToArray();
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            options.EmploymentRequired = true;
+            options.LD.IncludeHEFields = true;
+        }
+    }
+}
