5f1492ed6b4cc916de02a3f8ebb4ab2a4eec8483
Hema Jesu <Hema.Jesu@hotmail.com>
2018-10-15 10:01:58 +0100

    42645 - R101/ 42470- R29 /42565- R68

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/CrossEntity/R29.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class R29
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "R29";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "R29";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = Mutate, DoMutateOptions = MutateOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateProgType, DoMutateOptions = MutateOptions, ExclusionRecord = true },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.NonFunded, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptions, ExclusionRecord = true }
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+            if (!valid)
+            {
+                learner.LearningDelivery[0].FworkCodeSpecified = true;
+                learner.LearningDelivery[0].FworkCode = 403;
+                learner.LearningDelivery[0].LearnActEndDateSpecified = false;
+                learner.LearningDelivery[1].LearnActEndDateSpecified = false;
+            }
+        }
+
+        private void MutateProgType(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+            if (!valid)
+            {
+                learner.LearningDelivery[0].FworkCodeSpecified = true;
+                learner.LearningDelivery[0].FworkCode = 403;
+                learner.LearningDelivery[0].LearnActEndDateSpecified = true;
+                learner.LearningDelivery[0].LearnActEndDate = new DateTime(2017, 11, 30);
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            options.EmploymentRequired = true;
+            options.LD.GenerateMultipleLDs = 2;
+        }
+
+        private void MutateOptions(GenerationOptions options)
+        {
+        }
+    }
+}
