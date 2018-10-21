91d7cba8b6191d71b6c3b6cae7b53a5935896e54
Sanjeev Chevireddy <sanjeev.chevireddy@outlook.com>
2018-10-21 21:50:56 +0100

    61584 - FD-EngGrade_AP, 61824 - FD-MathGrade_AP, 61856 - FD-NINumber_AP, 62088 - FD-Sex_AP, 44273 - LearnDelFAMType_54, 44353 -  LearnDelFamType_71

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/LearnDelFAMType/LearnDelFAMType_54.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class LearnDelFAMType_54
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "LearnDelFAMType_54";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "LdfamTy54";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Adult, DoMutateLearner = Mutate1, DoMutateOptions = MutateGenerationOptions },
+            };
+        }
+
+        private void MutateEEF(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+            Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.EEF, LearnDelFAMCode.EEF_Apprenticeship_19);
+
+            if (valid)
+            {
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.FFI);
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.FFI, LearnDelFAMCode.FFI_Fully);
+            }
+        }
+
+        private void MutateFFI(MessageLearner learner, bool valid)
+        {
+            MutateEEF(learner, valid);
+            if (!valid)
+            {
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.FFI);
+            }
+        }
+
+        private void MutateApprenticeshipStandard(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+            ApprenticeshipProgrammeTypeAim pta = _dataCache.ApprenticeshipAims(ProgType.ApprenticeshipStandard).First();
+            Helpers.MutateApprenticeshipToStandard(learner, FundModel.OtherAdult);
+            Helpers.MutateDOB(learner, valid, Helpers.AgeRequired.Exact19, Helpers.BasedOn.LearnDelStart, Helpers.MakeOlderOrYoungerWhenInvalid.NoChange);
+            Helpers.SetApprenticeshipAims(learner, pta);
+            learner.LearningDelivery[0].LearnStartDate = new DateTime(2017, 05, 01).AddDays(-1); // FundModel_07
+            learner.LearnerEmploymentStatus[0].DateEmpStatApp = learner.LearningDelivery[0].LearnStartDate.AddDays(-1);
+            if (!valid)
+            {
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.EEF, LearnDelFAMCode.EEF_Apprenticeship_19);
+            }
+        }
+
+        private void Mutate19Standard(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+            Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.EEF, LearnDelFAMCode.EEF_Apprenticeship_19);
+            Helpers.MutateApprenticeshipToStandard(learner, FundModel.OtherAdult);
+        }
+
+        private void MutateIntermediateLevelApprenticeship(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+            Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.EEF, LearnDelFAMCode.EEF_Apprenticeship_19);
+            ApprenticeshipProgrammeTypeAim pta = _dataCache.ApprenticeshipAims(ProgType.IntermediateLevelApprenticeship).First();
+            Helpers.SetApprenticeshipAims(learner, pta);
+            foreach (var ld in learner.LearningDelivery)
+            {
+                ld.FundModel = (int)FundModel.OtherAdult;
+            }
+
+            if (valid)
+            {
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.FFI);
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.FFI, LearnDelFAMCode.FFI_Fully);
+            }
+        }
+
+        private void MutateApprenticeshipStandards(MessageLearner learner, bool valid)
+        {
+            ApprenticeshipProgrammeTypeAim pta = _dataCache.ApprenticeshipAims(ProgType.ApprenticeshipStandard).First();
+            Helpers.MutateApprenticeshipToStandard(learner, FundModel.OtherAdult);
+            Helpers.MutateDOB(learner, valid, Helpers.AgeRequired.Exact19, Helpers.BasedOn.LearnDelStart, Helpers.MakeOlderOrYoungerWhenInvalid.NoChange);
+            Helpers.SetApprenticeshipAims(learner, pta);
+            learner.LearningDelivery[0].LearnStartDate = new DateTime(2017, 05, 01).AddDays(-1); // FundModel_07
+            learner.LearnerEmploymentStatus[0].DateEmpStatApp = learner.LearningDelivery[0].LearnStartDate.AddDays(-1);
+            foreach (var ld in learner.LearningDelivery)
+            {
+                ld.ProgType = (int)ProgType.IntermediateLevelApprenticeship;
+            }
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            MutateEEF(learner, valid);
+            if (!valid)
+            {
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.EEF, LearnDelFAMCode.EEF_Apprenticeship_19);
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.FFI);
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.FFI, LearnDelFAMCode.FFI_Co);
+            }
+        }
+
+        private void Mutate1(MessageLearner learner, bool valid)
+        {
+            MutateEEF(learner, valid);
+            if (!valid)
+            {
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.EEF, LearnDelFAMCode.EEF_Apprenticeship_19);
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.FFI);
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.FFI, LearnDelFAMCode.FFI_Co);
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/LearnDelFAMType/LearnDelFAMType_71.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class LearnDelFAMType_71
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "LearnDelFAMType_71";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "LdfamTy71";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Adult, DoMutateLearner = MutateLearner, DoMutateOptions = MutateGenerationOptions },
+            };
+        }
+
+        private void MutateLearner(MessageLearner learner, bool valid)
+        {
+            learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-25);
+            Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.LDM, LearnDelFAMCode.LDM_Pilot);
+            if (valid)
+            {
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.ACT);
+                Helpers.AddLearningDeliveryFAM(learner, LearnDelFAMType.ACT, LearnDelFAMCode.ACT_ContractESFA);
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+        }
+    }
+}
