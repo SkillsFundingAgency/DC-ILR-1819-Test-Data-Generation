eff2112b519d6552ee9d58847a357b8c691b4af5
Amos-A <amos.awaghade@gmail.com>
2018-10-24 11:58:37 +0100

    ConRefNumber_01 WIP

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/ConRefNumber/ConRefNumber_01.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class ConRefNumber_01
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "ConRefNumber_01";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "ConRef01";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.ESF, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptionsDestProg }
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            var ld = learner.LearningDelivery;
+
+            foreach (MessageLearnerLearningDelivery lds in ld)
+            {
+                lds.SWSupAimId = Guid.NewGuid().ToString();
+                lds.ConRefNumber = "ESF-2010";
+                if (!valid) { lds.ConRefNumber = "ES-111112010"; }
+            }
+        }
+
+        private void MutateGenerationOptionsDestProg(GenerationOptions options)
+        {
+        }
+    }
+}
