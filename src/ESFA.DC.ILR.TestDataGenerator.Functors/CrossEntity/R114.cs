8b663f1676a427b7e257b73cce750e85d303bd93
Hema Jesu <Hema.Jesu@hotmail.com>
2018-10-31 10:06:15 +0000

    42655 - R104/42700 -R114/42520 -R56/42535 -R61/42540 - R62/ Add scenario to R108

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/CrossEntity/R114.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class R114
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+        private DateTime _outcomeDate;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "R114";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "R114";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = Mutate, DoMutateOptions = MutateGenerationOptionsLD2 },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateLearner, DoMutateOptions = MutateGenerationOptionsLD2, ExclusionRecord = true }
+            };
+        }
+
+        private void Mutate(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+                var lds = learner.LearningDelivery.ToList();
+                lds[1].FundModelSpecified = true;
+                lds[1].FundModel = 35;
+                lds[1].AimTypeSpecified = true;
+                lds[1].AimType = 3;
+                lds[1].LearnAimRef = "50079335"; //10005158
+            }
+        }
+
+        private void MutateLearner(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                learner.DateOfBirth = learner.LearningDelivery[0].LearnStartDate.AddYears(-19).AddMonths(-3);
+                var lds = learner.LearningDelivery.ToList();
+                lds[1].FundModelSpecified = true;
+                lds[1].FundModel = 35;
+                lds[1].AimTypeSpecified = true;
+                lds[1].AimType = 3;
+                lds[1].LearnAimRef = "10042702"; //10005158
+            }
+        }
+
+        private void MutateGenerationOptionsLD2(GenerationOptions options)
+        {
+            //options.CreateDestinationAndProgression = true;
+            options.EmploymentRequired = true;
+            _options = options;
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+            options.CreateDestinationAndProgression = true;
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/CrossEntity/R56.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class R56
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.July;
+        }
+
+        public string RuleName()
+        {
+            return "R56";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "R56";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateValidity, DoMutateOptions = MutateGenerateOptions },
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateAppreticeship, DoMutateOptions = MutateOptions, ExclusionRecord = true }
+            };
+        }
+
+        private void MutateLearner(MessageLearner learner, bool valid)
+        {
+            if (valid)
+            {
+                var lds = learner.LearningDelivery.ToList();
+                learner.LearningDelivery[1].LearnAimRef = "50104767";
+                learner.LearningDelivery[1].LearnStartDate = new DateTime(2017, 10, 14);
+                learner.LearningDelivery[1].AimTypeSpecified = true;
+                learner.LearningDelivery[1].AimType = 3;
+                learner.LearningDelivery[1].ProgTypeSpecified = true;
+                learner.LearningDelivery[1].ProgType = 2;
+                learner.LearningDelivery[1].PwayCodeSpecified = true;
+                learner.LearningDelivery[1].PwayCode = 1;
+                learner.LearningDelivery[1].FworkCodeSpecified = true;
+                learner.LearningDelivery[1].FworkCode = 420;
+            }
+        }
+
+        private void MutateValidity(MessageLearner learner, bool valid)
+        {
+            MutateLearner(learner, valid);
+            if (!valid)
+            {
+                learner.LearningDelivery[0].FundModelSpecified = true;
+                learner.LearningDelivery[0].FundModel = 99;
+                learner.LearningDelivery[1].LearnAimRef = "50104767";
+                learner.LearningDelivery[1].LearnStartDate = new DateTime(2017, 10, 14);
+                learner.LearningDelivery[1].AimTypeSpecified = true;
+                learner.LearningDelivery[1].AimType = 3;
+                learner.LearningDelivery[1].ProgTypeSpecified = true;
+                learner.LearningDelivery[1].ProgType = 2;
+                learner.LearningDelivery[1].PwayCodeSpecified = true;
+                learner.LearningDelivery[1].PwayCode = 1;
+                learner.LearningDelivery[1].FworkCodeSpecified = true;
+                learner.LearningDelivery[1].FworkCode = 420;
+                learner.LearningDelivery[1].FundModelSpecified = true;
+                learner.LearningDelivery[1].FundModel = 36;
+            }
+        }
+
+        private void MutateAppreticeship(MessageLearner learner, bool valid)
+        {
+            MutateLearner(learner, valid);
+            if (!valid)
+            {
+                learner.LearningDelivery[0].ProgTypeSpecified = true;
+                learner.LearningDelivery[0].FundModelSpecified = true;
+                learner.LearningDelivery[0].FundModel = 99;
+                learner.LearningDelivery[1].FundModelSpecified = true;
+                learner.LearningDelivery[1].FundModel = 99;
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.SOF);
+                Helpers.RemoveLearningDeliveryFAM(learner, LearnDelFAMType.ACT);
+            }
+        }
+
+        private void MutateOptions(GenerationOptions options)
+        {
+        }
+
+        private void MutateGenerateOptions(GenerationOptions options)
+        {
+        }
+    }
+}
