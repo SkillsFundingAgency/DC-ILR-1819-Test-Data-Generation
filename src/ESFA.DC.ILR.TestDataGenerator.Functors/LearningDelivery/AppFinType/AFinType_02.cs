9742a1a70a7360d62ee94025c1251b63af8df6dc
Amos-A <amos.awaghade@gmail.com>
2018-11-20 16:43:37 +0000

    Completed AFinType_02 and DateEmpStatApp_01

---
+++ src/ESFA.DC.ILR.TestDataGenerator.Functors/LearningDelivery/AppFinType/AFinType_02.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using DCT.ILR.Model;
+
+namespace DCT.TestDataGenerator.Functor
+{
+    public class AFinType_02
+        : ILearnerMultiMutator
+    {
+        private ILearnerCreatorDataCache _dataCache;
+        private GenerationOptions _options;
+
+        public FilePreparationDateRequired FilePreparationDate()
+        {
+            return FilePreparationDateRequired.None;
+        }
+
+        public string RuleName()
+        {
+            return "AFinType_02";
+        }
+
+        public string LearnerReferenceNumberStub()
+        {
+            return "Afinty02";
+        }
+
+        public IEnumerable<LearnerTypeMutator> LearnerMutators(ILearnerCreatorDataCache cache)
+        {
+            _dataCache = cache;
+            return new List<LearnerTypeMutator>()
+            {
+                new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateLearner, DoMutateOptions = MutateGenerationOptions },
+                //new LearnerTypeMutator() { LearnerType = LearnerTypeRequired.Apprenticeships, DoMutateLearner = MutateAdult, DoMutateOptions = MutateGenerationOptions, ExclusionRecord = true },
+            };
+        }
+
+        private void MutateLearner(MessageLearner learner, bool valid)
+        {
+            if (!valid)
+            {
+                learner.LearningDelivery[0].AppFinRecord[0].AFinCode = 5;
+            }
+        }
+
+        private void MutateGenerationOptions(GenerationOptions options)
+        {
+        }
+    }
+}
